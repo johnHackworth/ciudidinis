8b8b7fadbe9ab5e8ea1d960665b60a045e41537a
johnhackworth <javier.alvarez.lop@gmail.com>
2018-07-01 16:02:14 +0200

    Random tweets

--- app.js
+++ app.js
@@ -2,43 +2,79 @@ var Twitter = require( 'twitter' );
 var config = require( './config.js' );
 var mimimify = require( './mimimification.js' );
 
-let twitea = function() {
-	var twitterClient = new Twitter(config);
-	var params = {
-	  screen_name: 'albert_rivera',
-	  count: 1,
-	  result_type: 'recent',
-	  tweet_mode: 'extended',
-	  lang: 'es'
+const albertParams = {
+	screen_name: 'albert_rivera',
+	count: 1,
+	result_type: 'recent',
+	tweet_mode: 'extended',
+	lang: 'es',
+	trim_user: true,
+
+}	
+const twitterClient = new Twitter(config);
+
+const postTweet = function( id, origText ) {
+	const permalink = 'https://twitter.com/JohnHackworth/status/' + id;
+	const text = '"' + mimimify( origText ) + '"';
+	let params = {
+		status:  text + permalink,
 	}
-
-
+	twitterClient.post('statuses/update', params, function( err, response ) {
+	if(err){
+		console.log(err);
+	} else {
+		let username = response.user.screen_name;
+		let tweetId = response.id_str;      
+		}; 
+	} );
+}
+let checklastTweet = function() {
 	twitterClient.get( 
 		'statuses/user_timeline', 
-		params, 
+		albertParams, 
 		function( err, data, response ) {
 		  	if( !err ) {
 		  		for( let i = 0; i < data.length; i++ ) {
-		  			const permalink = 'https://twitter.com/JohnHackworth/status/' + data[ i ].id_str;
-		  			console.log( data[i] );
-		  			const text = '"' + mimimify( data[ i ].full_text ) + '"';
-				    let params = {
-		      			status:  text + permalink,
-			  		}
-		      		twitterClient.post('statuses/update', params, function( err, response ) {
-				        if(err){
-		        		  console.log(err);
-		        		} else {
-		          			let username = response.user.screen_name;
-		          			let tweetId = response.id_str;      
-		      			}; 
-		      		} );
+		  			postTweet( data[ i ].id_str, data[ i ].full_text );
 		      	} 
 		     } else {
 				console.log(err);
 	  		}
 		} 
 	);
+	let timeout = 30 * 60 * 1000 + Math.ceil( Math.random() * 30 * 60 * 1000 );
+	setTimeout( checklastTweet, timeout );	
+}
+let checkRandomTweet = function() {
+	let albertParamsRandom = {
+		screen_name: 'albert_rivera',
+		count: 1,
+		result_type: 'recent',
+		tweet_mode: 'extended',
+		lang: 'es',
+		count: Math.ceil( 3200 * Math.random() ),
+		exclude_replies: true,
+		trim_user: true, 	
+	}
+	twitterClient.get( 
+		'statuses/user_timeline', 
+		albertParamsRandom, 
+		function( err, data, response ) {
+		  	if( !err ) {
+	  			postTweet( data[ data.length - 1 ].id_str, data[ data.length - 1 ].full_text );
+		     } else {
+				console.log(err);
+	  		}
+		} 
+	);
+	let timeout2 = 30 * 60 * 1000 + Math.ceil( Math.random() * 30 * 60 * 1000 );
+	setTimeout( checkRandomTweet, timeout2 );
 }
 
-setInterval( twitea, 30000 );
+let start = function() {
+	console
+	checklastTweet();
+	checkRandomTweet();
+}
+
+start();
